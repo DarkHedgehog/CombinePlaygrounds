dbfa93a2a86efd0319f58dd8f7c180f005e76f7f
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-05-13 13:54:10 +0300

    Initial Commit

---
+++ CombinePlaygrounds/CombinePlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePlaygroundsApp.swift
+//  CombinePlaygrounds
+//
+//  Created by Aleksandr Derevenskih on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombinePlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
