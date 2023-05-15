622fbdc3e73b8e6bc5b8cd5419a220451c2b543e
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-05-15 13:17:14 +0300

    publish, subscribe playing

--- CombinePlaygrounds.playground/Contents.swift
+++ CombinePlaygrounds.playground/Contents.swift
@@ -1,5 +1,30 @@
 import UIKit
+import Combine
 
-var greeting = "Hello, playground"
 
+let center = NotificationCenter.default
+let myNotification = Notification.Name("SimpleTypeNotification")
+let publisher = NotificationCenter.default .publisher(for: myNotification, object: nil)
 
+let observer = center.addObserver(
+    forName: myNotification, object: nil, queue: nil) { notification in
+        print("SimpleTypeNotification received!")
+    }
+
+let subscription = publisher.sink{ value in
+    print("subscription \(value.object) received!")
+}
+
+center.post(name: myNotification, object: nil) // 6
+center.post(name: myNotification, object: 1) // 6
+center.post(name: myNotification, object: "123aa") // 6
+center.removeObserver(observer)
+subscription.cancel()
+
+let just = Just("Hello world!")
+
+_ = just.sink(receiveCompletion: {
+    print("just Received completion", $0)
+}, receiveValue: {
+    print("just Received value", $0)
+})
