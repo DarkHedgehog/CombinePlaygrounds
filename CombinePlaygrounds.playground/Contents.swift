5119dd1dcbc6aaad4074da6528cf527e3e164908
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-05-25 12:16:02 +0300

    network playing

--- CombinePlaygrounds.playground/Contents.swift
+++ CombinePlaygrounds.playground/Contents.swift
@@ -7,13 +7,65 @@ public func main(action: () -> Void) {
 }
 
 main {
-    guard let url = URL(string: "https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com") else { return }
+    var subscriptions = Set<AnyCancellable>()
 
-    let subscription = URLSession.shared .dataTaskPublisher(for: url) .sink(receiveCompletion: { completion in
-        if case .failure(let err) = completion {
-            print("Retrieving data failed with error \(err)")
+    let publisher = availableProducts()
+
+    let subscription1 = publisher
+        .sink(receiveCompletion: { completion in
+            if case .failure(let err) = completion {
+                print("Retrieving data failed with error \(err)")
+            }
+        }, receiveValue: { products in
+            print("Retrieved products of size \(products.count)")
+            products.forEach {
+                print ("\($0.name): \($0.price)")
+            }
+        })
+        .store(in: &subscriptions)
+
+    let subscription2 = publisher
+        .sink(receiveCompletion: { completion in
+            if case .failure(let err) = completion {
+                print("Retrieving data failed with error \(err)")
+            }
+        }, receiveValue: { products in
+            print("Retrieved products of size \(products.count)")
+            products.forEach {
+                print ("\($0.name): \($0.price)")
+            }
+        })
+        .store(in: &subscriptions)
+
+    CFRunLoopRun()
+
+    func availableProducts() -> AnyPublisher<[ProductShort], any Error> {
+        guard let url = URL(string: "https://swiftcleancodemock.onrender.com/products") else {
+            return AnyPublisher(
+                Fail<[ProductShort], Error>(error: URLError(.cannotConnectToHost))
+            )
         }
-    }, receiveValue: { data, response in
-        print("Retrieved data of size \(data.count), response = \(response)") }
-    )
+
+        let publisher = URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .print()
+            .decode(type: [ProductShort].self, decoder: JSONDecoder())
+            .print()
+            .share()
+            .eraseToAnyPublisher()
+
+        return publisher
+    }
+
+    struct ProductShort: Codable, Identifiable {
+        let id: UUID
+        let name: String
+        let price: Double
+        enum CodingKeys: String, CodingKey {
+            case id = "id"
+            case name = "name"
+            case price = "cost"
+        }
+    }
+
 }
