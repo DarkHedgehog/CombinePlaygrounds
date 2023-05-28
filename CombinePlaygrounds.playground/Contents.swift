a3dd86ff4bf87256aa07e77761e5cc68134f2f72
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-05-28 14:18:16 +0300

    5

--- CombinePlaygrounds.playground/Contents.swift
+++ CombinePlaygrounds.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Combine
+import SwiftUI
 
 
 public func main(action: () -> Void) {
@@ -8,36 +9,37 @@ public func main(action: () -> Void) {
 
 main {
     var subscriptions = Set<AnyCancellable>()
+    var data = TestClass()
 
-    let publisher = availableProducts()
+    timerStart(timeRange: 1...1000) {
+        let publisher = availableProducts()
+        publisher
+            .sink(receiveCompletion: { completion in
+                if case .failure(let err) = completion {
+                    print("Retrieving data failed with error \(err)")
+                }
+            }, receiveValue: { products in
+                print("Retrieved products of size \(products.count)")
+                data.model.products = products
+            })
+            .store(in: &subscriptions)
+    }
 
-    let subscription1 = publisher
-        .sink(receiveCompletion: { completion in
-            if case .failure(let err) = completion {
-                print("Retrieving data failed with error \(err)")
-            }
-        }, receiveValue: { products in
-            print("Retrieved products of size \(products.count)")
-            products.forEach {
-                print ("\($0.name): \($0.price)")
-            }
-        })
-        .store(in: &subscriptions)
 
-    let subscription2 = publisher
-        .sink(receiveCompletion: { completion in
-            if case .failure(let err) = completion {
-                print("Retrieving data failed with error \(err)")
-            }
-        }, receiveValue: { products in
-            print("Retrieved products of size \(products.count)")
-            products.forEach {
-                print ("\($0.name): \($0.price)")
-            }
-        })
-        .store(in: &subscriptions)
+    data.model.$products.sink {
+        print("---")
+        print($0)
+    }
 
     CFRunLoopRun()
+
+    func timerStart(timeRange: ClosedRange<Int>, handler: @escaping () -> Void) -> Timer {
+        let interval = Double(Int.random(in: timeRange)) / 1000
+        let timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: false) { timer in
+            handler()
+        }
+        return timer
+    }
 
     func availableProducts() -> AnyPublisher<[ProductShort], any Error> {
         guard let url = URL(string: "https://swiftcleancodemock.onrender.com/products") else {
@@ -56,7 +58,6 @@ main {
 
         return publisher
     }
-
     struct ProductShort: Codable, Identifiable {
         let id: UUID
         let name: String
@@ -68,4 +69,11 @@ main {
         }
     }
 
+    class ModelData: ObservableObject {
+        @Published var products: [ProductShort] = []
+    }
+
+    class TestClass {
+        @ObservedObject var model = ModelData()
+    }
 }
